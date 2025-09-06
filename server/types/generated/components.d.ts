f46cde171cd49d2ee42690864ef2d4449f343216
Christopher Owens <cowensdev@gmail.com>
2025-09-06 13:55:40 -0400

    Halfway commit

--- server/types/generated/components.d.ts
+++ server/types/generated/components.d.ts
@@ -8,8 +8,29 @@ export interface BlocksFeaturedArticle extends Struct.ComponentSchema {
   attributes: {
     excerpt: Schema.Attribute.RichText;
     headline: Schema.Attribute.String;
-    image: Schema.Attribute.Media<'images' | 'files'>;
+    image: Schema.Attribute.Media<'images'>;
     link: Schema.Attribute.Component<'elements.link', false>;
+  };
+}
+
+export interface BlocksFullImage extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_full_images';
+  info: {
+    displayName: 'Full Image';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<'images'>;
+  };
+}
+
+export interface BlocksHeading extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_headings';
+  info: {
+    displayName: 'Heading';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    linkId: Schema.Attribute.String;
   };
 }
 
@@ -21,7 +42,7 @@ export interface BlocksHeroSection extends Struct.ComponentSchema {
   attributes: {
     cta: Schema.Attribute.Component<'elements.link', false>;
     heading: Schema.Attribute.String;
-    image: Schema.Attribute.Media<'images' | 'files'>;
+    image: Schema.Attribute.Media<'images'>;
     logo: Schema.Attribute.Component<'elements.logo', false>;
     theme: Schema.Attribute.Enumeration<['turquoise', 'orange']>;
   };
@@ -36,9 +57,32 @@ export interface BlocksInfoBlock extends Struct.ComponentSchema {
     content: Schema.Attribute.RichText;
     cta: Schema.Attribute.Component<'elements.link', false>;
     headline: Schema.Attribute.String;
-    image: Schema.Attribute.Media<'images' | 'files'>;
+    image: Schema.Attribute.Media<'images'>;
     reversed: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
     theme: Schema.Attribute.Enumeration<['turquoise', 'orange']>;
+  };
+}
+
+export interface BlocksParagraph extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_paragraphs';
+  info: {
+    displayName: 'Paragraph';
+  };
+  attributes: {
+    content: Schema.Attribute.RichText;
+  };
+}
+
+export interface BlocksParagraphWithImage extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_paragraph_with_images';
+  info: {
+    displayName: 'Paragraph With Image';
+  };
+  attributes: {
+    content: Schema.Attribute.RichText;
+    image: Schema.Attribute.Media<'images'>;
+    imageLandscape: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<true>;
+    reversed: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
   };
 }
 
@@ -107,8 +151,12 @@ declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'blocks.featured-article': BlocksFeaturedArticle;
+      'blocks.full-image': BlocksFullImage;
+      'blocks.heading': BlocksHeading;
       'blocks.hero-section': BlocksHeroSection;
       'blocks.info-block': BlocksInfoBlock;
+      'blocks.paragraph': BlocksParagraph;
+      'blocks.paragraph-with-image': BlocksParagraphWithImage;
       'blocks.subscribe': BlocksSubscribe;
       'elements.link': ElementsLink;
       'elements.logo': ElementsLogo;
